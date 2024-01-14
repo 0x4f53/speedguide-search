79e9a46c7dae41761f01112520cca2a38743cee0
0x4f53 <71916237+0x4f53@users.noreply.github.com>
2024-01-15 03:36:25 +0530

    backup

--- scan_port.py
+++ scan_port.py
@@ -1,47 +1,20 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from webdriver_manager.chrome import ChromeDriverManager
-import time, os, traceback
-from selenium.webdriver.chrome.service import Service
-from bs4 import BeautifulSoup
+import utils
 
-from PIL import Image
+port = 8080
 
-def scrape_page (url):
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    driver = webdriver.Chrome(                      
-        service=Service(
-            executable_path=ChromeDriverManager().install()
-        ),
-        options=options
-    )
-    
-    source =""
-    
-    try:  
-        print (f"Fetching page from {url}")
-        driver.get(url)
-        time.sleep(2)
-        
-        source = driver.page_source
+timestamp = None
 
-    except:
-        print ("An error occurred:")
-        traceback.print_exc()
-        driver.quit()
-        
-    return source
+try:
+    timestamp = utils.getLastScanTimestamp(port)
+except:
+    timestamp = utils.setLastScanTimestamp(port)
 
-def grab_port_table(html_source):
-    soup = BeautifulSoup(html_source, 'html.parser')
-    table = soup.find('table', class_='port-outer')
-    rows = []
-    for row in table.find_all('tr'):
-        cells = [cell.text for cell in row.find_all(['th', 'td'])]
-        rows.append(cells)
-    for row in rows: print(row[0])  
 
-source = scrape_page("https://www.speedguide.net/port.php?port=443")
-grab_port_table(source)
+
+print(utils.setLastScanTimestamp(port))
+
+
+
+#source = utils.scrapePage(f"https://www.speedguide.net/port.php?port={port}")
+#utils.saveData(source, f"{port}.portdata")
+#utils.grabPortTable(source)
--- utils.py
+++ utils.py
@@ -1,12 +1,89 @@
-import os
+import os, datetime, traceback
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+import time, os, traceback
+from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup
 
-def substringAfter(text, substring): return text.split(substring, 1)[-1]
+directory = ".cache/"
+file_name_extension = ".time"
 
-def save_data(data, file_name):
-    directory = "."
+def getCurrentTimestamp(): return datetime.datetime.fromtimestamp(int(datetime.datetime.now().timestamp()), tz=datetime.timezone.utc) 
+
+def getLastScanTimestamp(filename) -> datetime:
+    
+    timestamp = None
+    
+    if not os.path.exists(directory): 
+        raise FileNotFoundError()
+    
+    with open (directory + "/" + str(filename) + file_name_extension, "r") as data_file:
+        timestamp = datetime.datetime.fromtimestamp(int(data_file.read()), tz=datetime.timezone.utc)
+        data_file.close()
+    
+    return timestamp
+
+def setLastScanTimestamp(filename) -> datetime:
+    
     if not os.path.exists(directory): 
         os.mkdir(directory)
         print (f"Created a cache directory at: {os.path.abspath(directory)}")
     
+    timestamp = int(datetime.datetime.now().timestamp())
+    
+    with open (directory + "/" +  str(filename) + file_name_extension, "w") as data_file:
+        data_file.write(str(timestamp))
+        data_file.close()
+    
+    return datetime.datetime.fromtimestamp(timestamp, tz=datetime.timezone.utc)  
+
+def substringAfter(text, substring): return text.split(substring, 1)[-1]
+
+def saveData(data, file_name):
+    directory = ".cache/"
+    if not os.path.exists(directory): 
+        os.mkdir(directory)
+        print (f"Created a cache directory at: {os.path.abspath(directory)}")
+        
+    with open (directory + "/" + file_name, "w") as data_file:
+        data_file.write(data)
+        data_file.close()
+    
     return True
     
+def scrapePage (url):
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(                      
+        service=Service(
+            executable_path=ChromeDriverManager().install()
+        ),
+        options=options
+    )
+    
+    source =""
+    
+    try:  
+        print (f"Fetching page from {url}")
+        driver.get(url)
+        time.sleep(2)
+        
+        source = driver.page_source
+
+    except:
+        print ("An error occurred:")
+        traceback.print_exc()
+        driver.quit()
+        
+    return source
+
+def grabPortTable(html_source):
+    soup = BeautifulSoup(html_source, 'html.parser')
+    table = soup.find('table', class_='port-outer')
+    rows = []
+    for row in table.find_all('tr'):
+        cells = [cell.text for cell in row.find_all(['th', 'td'])]
+        rows.append(cells)
+    for row in rows: print(row[0])  
